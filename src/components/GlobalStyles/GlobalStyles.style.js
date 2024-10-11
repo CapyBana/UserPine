c43577837e07a278036821f6c91b46ca9a7be927
KhoaAnhNguyen04 <khoanguyenanh040104@gmail.com>
2024-10-11 12:27:45 +0700

    remove unuse file -- update global var

--- src/components/GlobalStyles/GlobalStyles.style.js
+++ src/components/GlobalStyles/GlobalStyles.style.js
@@ -3,22 +3,33 @@ import normalize from 'normalize.css';
 
 const GlobalStyles = createGlobalStyle`
   ${normalize}
-  *, *::after, *::before{
-        margin:0;
-        padding:0;
-        box-sizing: border-box;
+  @import url('https://fonts.googleapis.com/css2?family=Unbounded&display=swap');
+
+  *, *::after, *::before {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
   }
+
   :root {
     --primary-red: #e12117; 
     --primary-red-hover: #ff6347; 
-    --primary-black: #D9D9D9
+    --primary-black: #D9D9D9;
+    --boundedFont: 'Unbounded', sans-serif;
+    
+    --primary-text_size: '40px'
+    --title-text_size: '32px'
+    --normal-text_size: '16px'
   }
 
   html {
     height: 100%;
-}
+    .app {
+      width: 100%;
+    }
+  }
+
   body {
-    font-family: 'Roboto', sans-serif;
     font-size: 1.6rem;
     line-height: 1.5;
     min-height: 100vh; 

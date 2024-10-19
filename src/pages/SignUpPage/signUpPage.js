c28377b7d852c8230549371d15e6851c181dbf0d
DESKTOP-TSQE45N\tiend <dat.tran0912@hcmut.edu.vn>
2024-10-19 13:47:09 +0700

    Update Sign Up Page 1.2

--- src/pages/SignUpPage/signUpPage.js
+++ src/pages/SignUpPage/signUpPage.js
@@ -1,37 +1,41 @@
 import React from 'react';
-import { BackToHomePage, Capybana, FillInBox,  InputBox,  MainContent, SignInBox, SubmitButton, ThirdContent } from './signUpPage.style';
+import { BackToHomePage, Capybana, Content, FillInBox,  InputBox,  MainContent, SignInBox, SignInPage, SubmitButton} from './signUpPage.style';
 import Boxicons from 'boxicons';
 const SignUpPage = () => {
     return (
         <div>
-            <a href = "log-in">
-                <BackToHomePage>⬅ Back to Home page</BackToHomePage>
-            </a>
-            <SignInBox>
-                <ThirdContent>Be a part of <Capybana>Capybana Community</Capybana></ThirdContent>
-                <MainContent>Sign up</MainContent>
-                <form action = "somewhere" method = "POST">
-                    <InputBox style = {{position: "relative"}}>
-                        <FillInBox type = "text" id = "username" placeholder = "Username" minlength = "6" maxlength = "30" required></FillInBox>
-                        <box-icon style = {{position: "absolute", top: "32.5%", right: "20px"}} type='solid' name='user' size="sm"></box-icon>
-                    </InputBox><br></br>
-                    <InputBox style = {{position: "relative"}}>
-                        <FillInBox type = "email" id = "email" placeholder = "Email" required></FillInBox>
-                        <box-icon style = {{position: "absolute", top: "32.5%", right: "20px"}}  type='solid' name='envelope' size="sm"></box-icon>
-                    </InputBox><br></br>
-                    <InputBox style = {{position: "relative"}}>
-                        <FillInBox type = "password" id = "password" placeholder = "Password" minlength = "8" maxlength = "30" required></FillInBox>
-                        <box-icon style = {{position: "absolute", top: "32.5%", right: "20px"}}  type='solid' name='lock-alt' size="sm"></box-icon>
-                    </InputBox><br></br>
-                    <InputBox style = {{position: "relative"}}>
-                        <FillInBox type = "password" id = "cfpassword" placeholder = "Comfirm Password" minlength = "8" maxlength = "30" required></FillInBox>
-                        <box-icon style = {{position: "absolute", top: "32.5%", right: "20px"}}  type='solid' name='lock-alt' size="sm"></box-icon>
-                    </InputBox><br></br>
-                    <text>Already have an account? <a href = "log-in">Log in</a></text><br></br>
+            <SignInPage>
+                <a href = "log-in">
+                    <Content>⬅ Back to Home page</Content>
+                </a>
+                <SignInBox>
+                    <Content>Be a part of <Capybana>Capybana Community</Capybana></Content>
+                    <MainContent>Sign up</MainContent>
+                    <div style = {{size: "75%"}}>
+                    <form action = "somewhere" method = "POST">
+                            <InputBox style = {{position: "relative"}}>
+                                <FillInBox type = "text" id = "username" placeholder = "Username" minlength = "6" maxlength = "30" required></FillInBox>
+                                <box-icon style = {{position: "absolute", top: "26%", right: "20px"}} type='solid' name='user' size="sm"></box-icon>
+                            </InputBox>
+                            <InputBox style = {{position: "relative"}}>
+                                <FillInBox type = "email" id = "email" placeholder = "Email" required></FillInBox>
+                                <box-icon style = {{position: "absolute", top: "26%", right: "20px"}}  type='solid' name='envelope' size="sm"></box-icon>
+                            </InputBox>
+                            <InputBox style = {{position: "relative"}}>
+                                <FillInBox type = "password" id = "password" placeholder = "Password" minlength = "8" maxlength = "30" required></FillInBox>
+                                <box-icon style = {{position: "absolute", top: "26%", right: "20px"}}  type='solid' name='lock-alt' size="sm"></box-icon>
+                            </InputBox>
+                            <InputBox style = {{position: "relative"}}>
+                                <FillInBox type = "password" id = "cfpassword" placeholder = "Confirm Password" minlength = "8" maxlength = "30" required></FillInBox>
+                                <box-icon style = {{position: "absolute", top: "26%", right: "20px"}}  type='solid' name='lock-alt' size="sm"></box-icon>
+                            </InputBox>
+                            <Content>Already have an account? <a href = "log-in">Log in</a></Content><br></br>
 
-                    <SubmitButton type = "submit" id = "submitButton">Sign up</SubmitButton>
-                </form>
-            </SignInBox>
+                            <SubmitButton type = "submit" id = "submitButton">Sign up</SubmitButton>
+                        </form>
+                    </div>
+                </SignInBox>
+            </SignInPage>
         </div>
     );
 }
--- src/pages/SignUpPage/signUpPage.style.js
+++ src/pages/SignUpPage/signUpPage.style.js
@@ -1,43 +1,46 @@
 import styled from 'styled-components';
 
+export const SignInPage = styled.div`
+    
+`;
 export const SubmitButton = styled.button`
     height: 100%;
-    padding: 4% 10%;
+    padding: 2% 5%;
     margin: 2%;
     background-color: #1D269A;
     color: #FAE6E5;
     border-radius: 45px;
-    font-size: 1.3em;
+    font-size: var(--large-text_size);
     text-align: center;
     cursor: pointer;
     border: none;
 `;
 export const SignInBox = styled.div`
     width: 50%;
-    font-family: unbounded, arial;
+    font-family: arial;
     background-color: #100C0C;
     color: #FAE6E5;
     justify-content: space-between;
     text-align: center;
     border-radius: 45px;
-    padding: 10px 10px;
     margin-left: auto;
     margin-right: auto;
     margin-top: auto;
-    margin-bottome: auto;
-    padding: 5% 10%;
+    margin-bottom: auto;
+    padding: 2% 5% 2% 5%;
 `;
 export const MainContent = styled.h1`
     text-align: center;
     color: #FAE6E5;
     font-style: strong;
-    font-size: 2.5em;
+    font-size: var(--super-text_size);
+    margin-top: 0;
+    margin-bottom: 1%;
 `;
-export const ThirdContent = styled.h3`
+export const Content = styled.text`
     text-align: center;
     color: #FAE6E5;
-    font-family: Times New Roman;
-    forn-size: 1.5em;
+    font-size: var(--medium-text_size);
 `;
 export const InputBox = styled.div`
     width: 100%;
@@ -46,27 +49,30 @@ export const InputBox = styled.div`
     align-content: center;
     color: #FAE6E5;
     border: none;
-    border-radius: 20px;
+    border-radius: 15px;
     margin-left: auto;
     margin-right: auto;
+    margin-bottom: 2%;
 `
 export const FillInBox = styled.input`
-    padding: 20px 45px 20px 20px;
+    padding: 2% 9% 2% 4%;
     width: 100%;
     height: 100%;
     background-color: transparent;
+    font-size: var(--medium-text_size);
     color: #FAE6E5;
     outline: none;
     border: none;
 `;
 export const BackToHomePage = styled.button`
     background-color: transparent;
-    margin: 20px 25px;
+    margin: 2% 2.5%;
     color: #DCD1D1;
     border: none;
     cursor: pointer;
 `;
-export const Capybana = styled.h3`
-    font-family: fantasy;
+export const Capybana = styled.text`
+    font-family: algerian, fantasy;
     justify-content: center;
+    font-size: var(--large-text_size);
 `;

a7f212540b3266972a3a7d52a4f24d0d45f36183
khanhmin <khminhcanfly@gmail.com>
2024-10-28 15:12:19 +0700

    [Log-in] Create Log In screen
    Co-authored-by: KhoaAnhNguyen04 <khoanguyenanh040104@gmail.com>

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,16 @@
 import Dashboard from '~/pages/Dashboard/Dashboard';
 import Review from '~/pages/Review/Review';
 import SignUpPage from '~/pages/SignUpPage/signUpPage';
+import LogInPage from '~/pages/LogInPage/LogInPage';
 import UserProfile from '~/pages/UserProfile/userProfile';
 
 const routes = [
     { path: '/', component: Dashboard },
     { path: '/review-post', component: Review, layout: null },
     { path: '/sign-up', component: SignUpPage},
+    { path: '/log-in', component: LogInPage}
     { path: '/user-profile', component: UserProfile, layout: null},
 ];
+
 
 export { routes };

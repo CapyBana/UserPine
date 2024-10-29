492c890e098f567e3369789e030cc7fe43fb99dd
CuongChuck <englishtalent123@gmail.com>
2024-10-30 00:13:56 +0700

    Update & Fix F4: ReviewForm

---
+++ src/pages/Review/Review.style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const ReviewPage = styled.div`
+    background-color: #000000;
+    display: flex;
+    flex-direction: column;
+    color: #FAE6E5;
+    font-family: var(--boundedFont);
+    height: 1500px;
+`;

963b668df2ab73099a4078c0dfb429afaaff4157
Trí Nguyễn <>
2024-11-21 10:08:39 +0700

    Thẻ search result

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -16,21 +16,16 @@ const Navbar = (props) => {
             <SearchContainer>
                 <SearchInput type="text" placeholder="Search..." />
             </SearchContainer>
-            <MenuItem to="/about">Categories</MenuItem>
             <HamburgerIcon onClick={toggleMenu}>
                 <span></span>
                 <span></span>
                 <span></span>
             </HamburgerIcon>
             <Menu isOpen={isOpen}>
-<<<<<<< HEAD
-                <MenuItem to="/sign-up">Sign Up</MenuItem>
-                <MenuItem to="/log-in">Log In</MenuItem>
-=======
                 <MenuItem onClick={() => props.handlePage('/')} to="/">
                     Home
                 </MenuItem>
-                <MenuItem to="/about">
+                <MenuItem to="/">
                     <ul>
                         <li>
                             Categories
@@ -62,7 +57,6 @@ const Navbar = (props) => {
                 </MenuItem>
                 <MenuItem onClick={() => props.handlePage('sign-up')}>Sign Up</MenuItem>
                 <MenuItem onClick={() => props.handlePage('log-in')}>Log In</MenuItem>
->>>>>>> d6c7a661bb1c4a0239d0f6b39dfac8ab3d73edfc
             </Menu>
         </NavbarContainer>
     );

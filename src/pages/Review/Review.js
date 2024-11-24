d162d4884086c9f4a2bc1ee38ec10053ccbf2ade
DESKTOP-4T1N182\LENOVO <khminhcanfly@gmail.com>
2024-11-24 20:13:19 +0700

    topMovie i

--- src/pages/Review/Review.js
+++ src/pages/Review/Review.js
@@ -1,22 +1,22 @@
-import ReviewForm from "~/components/ReviewForm/ReviewForm";
-import { ReviewLayout } from "./Review.style";
-import ReturnBlock from "~/components/ReturnLink/Return";
-import { Link } from "react-router-dom";
+//import ReviewForm from "~/components/ReviewForm/ReviewForm";
+//import { ReviewLayout } from "./Review.style";
+//import ReturnBlock from "~/components/ReturnLink/Return";
+//import { Link } from "react-router-dom";
 
-const Review = () => {
-export default function ReviewPage() {
-    return (
-        <ReviewLayout>
-            <Link to='/' style={{ 
-                textDecoration:'none', 
-                width:'400px', 
-                padding:'80px 2.5% 15px' 
-            }}>
-                <ReturnBlock backmessage="Back to homepage" />
-            </Link>
-            <ReviewForm name="Movie Title" />
-        </ReviewLayout>
-    );
-}
+//const Review = () => {
+//export default function ReviewPage() {
+    //return (
+        //<ReviewLayout>
+            //<Link to='/' style={{ 
+                //textDecoration:'none', 
+                //width:'400px', 
+                //padding:'80px 2.5% 15px' 
+            //}}>
+                //<ReturnBlock backmessage="Back to homepage" />
+            //</Link>
+            //<ReviewForm name="Movie Title" />
+        //</ReviewLayout>
+    //);
+//}
 
-export default Review;
+//export default Review;
--- src/pages/Review/Review.style.js
+++ src/pages/Review/Review.style.js
@@ -8,3 +8,12 @@ export const ReviewPage = styled.div`
     font-family: var(--boundedFont);
     height: 970px;
 `;
+
+export const ReviewLayout = styled.div`
+    background-color: #181818;
+    display: flex;
+    flex-direction: column;
+    color: #FAE6E5;
+    font-family: var(--boundedFont);
+    height: 970px;
+`;

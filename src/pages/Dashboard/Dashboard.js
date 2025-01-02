c953cfd8d0285b0681fdddaff0b47771681f29c9
DESKTOP-TSQE45N\tiend <dat.tran0912@hcmut.edu.vn>
2025-01-02 21:01:34 +0700

    Comment in Dashboard

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -5,7 +5,6 @@ import TranslateMvCard from '~/components/MvCardEffect/TranslateMvCard';
 import VerticalMvList from '~/components/VerticalMvList/VerticalMvList';
 import narutoImg from 'src/assets/images/naruto.png';
 import onePieceImg from 'src/assets/images/one_piece.png';
-import { CommentBlock, UserComment } from './Dashboard.style';
 import { MvRating } from '~/components/VerticalMvCard/VerticalMvCard.style';
 import { MovieImg, MovieTitle } from '~/components/ReviewForm/ReviewForm.style';
 
@@ -16,7 +15,41 @@ export const DashboardLayout = styled.div`
     display: flex;
     flex-direction: row;
 `;
-
+export const CommentBlock = styled.div`
+    margin-top: 65px;
+    width: 90%;
+    display: flex;
+    flex-direction: column;
+    background-color: black;
+    padding: 20px;
+    border-radius: 20px;
+    color: var(--primary-white);
+    h3 {
+        padding: 0 0 20px 0;
+    }
+`;
+export const UserComment = styled.div`
+    width: 100%;
+    background-color: #181818;
+    padding: 10px;
+    .head {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+    .title {
+        padding-left: 20px;
+    }
+    p {
+        font-size: var(--normal-text_size);
+        height: 150px;
+        width: auto;
+        overflow-y: hidden;
+    }
+    text {
+        font-size: var(--medium-text_size);
+    }
+`
 export default function Dashboard() {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);

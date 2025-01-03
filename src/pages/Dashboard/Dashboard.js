dc1afc2a8e1f1b63f90ccafe15fa9fce813a3ed5
tiendattranle <tiendattranle0912@gmail.com>
2025-01-03 08:17:58 +0700

    Tien dat/comment (#32)
    * Showing comment
    
    * Comment in Dashboard
    
    ---------
    
    Co-authored-by: DESKTOP-TSQE45N\tiend <dat.tran0912@hcmut.edu.vn>
    Co-authored-by: KhoaAnhNguyen04 <khoanguyenanh040104@gmail.com>

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -6,14 +6,51 @@ import VerticalMvList from '~/components/VerticalMvList/VerticalMvList';
 import { LoginContext } from '~/context/loginContext';
 import narutoImg from 'src/assets/images/naruto.png';
 import onePieceImg from 'src/assets/images/one_piece.png';
+import { MvRating } from '~/components/VerticalMvCard/VerticalMvCard.style';
+import { MovieImg, MovieTitle } from '~/components/ReviewForm/ReviewForm.style';
 
 export const DashboardLayout = styled.div`
     width: 100%;
+    margin-top: 65px;
     padding: 0 5%;
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
 `;
-
+export const CommentBlock = styled.div`
+    margin-top: 65px;
+    width: 90%;
+    display: flex;
+    flex-direction: column;
+    background-color: black;
+    padding: 20px;
+    border-radius: 20px;
+    color: var(--primary-white);
+    h3 {
+        padding: 0 0 20px 0;
+    }
+`;
+export const UserComment = styled.div`
+    width: 100%;
+    background-color: #181818;
+    padding: 10px;
+    .head {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+    .title {
+        padding-left: 20px;
+    }
+    p {
+        font-size: var(--normal-text_size);
+        height: 150px;
+        width: auto;
+        overflow-y: hidden;
+    }
+    text {
+        font-size: var(--medium-text_size);
+    }
+`
 export default function Dashboard() {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
@@ -58,7 +95,40 @@ export default function Dashboard() {
 
     return (
         <DashboardLayout>
-            <VerticalMvList title="top Movie" data={hardData} />
+            <div style={{display: "flex", flexDirection: "column"}}>
+                <VerticalMvList title="Top Movie" data={hardData} />
+                <CommentBlock>
+                    <h3>Comment</h3>
+                    <UserComment>
+                        <text>Username</text>
+                        <div style={{display: "flex", flexDirection: "row"}}>
+                            <div style={{backgroundColor: "#1f1f1f", width: "1px", height: "auto", margin: "0 20px"}}></div>
+                            <div>
+                                <div class="head">
+                                    <MovieImg src={narutoImg} alt="Movie Picture" 
+                                        style={{width: "100px"}}/>
+                                    <div class="title">
+                                        <MovieTitle>Movie title</MovieTitle>
+                                        <MvRating
+                                            size="medium"
+                                            name="rt"
+                                            value={rating}
+                                            precision={0.5}
+                                            readOnly
+                                        />
+                                    </div>
+                                </div>
+                                <text>Review title</text>
+                                <p>Lorem nkjdvnkjdnfvksndvndsjkvnnesergeeriegbijreijbijbjLorem nkjdvnkjdnfvksndvndsjkvnnesergeeriegbijreijbijbjLorem nkjdvnkjdnfvksndvndsjkvnnesergeeriegbijreijbijbjLorem nkjdvnkjdnfvksndvndsjkvnnesergeeriegbijreijbijbjLorem nkjdvnkjdnfvksndvndsjkvnnesergeeriegbijreijbijbjLorem nkjdvnkjdnfvksndvndsjkvnnesergeeriegbijreijbijbjLorem nkjdvnkjdnfvksndvndsjkvnnesergeeriegbijreijbijbjLorem nkjdvnkjdnfvksndvndsjkvnnesergeeriegbijreijbijbjLorem nkjdvnkjdnfvksndvndsjkvnnesergeeriegbijreijbijbjLorem nkjdvnkjdnfvksndvndsjkvnnesergeerie
+                                gbijreijbijbjLorem nkjdvnkjdnfvksndvndsjkvnnesergeeriegbijreijbijbjLorem nkjdvnkjdnfvksndvndsjkvnnesergeeriegbijreijbijbjLorem nkjdvnkjdnfvksndvndsjkvnnesergeeriegbijreijbijbjLorem nkjdvnkjdnfvksndvndsjkvnnesergeeriegbijreijbijbj
+                                Lorem nkjdvnkjdnfvksndvndsjkvnnesergeeriegbijreijbijbjLorem nkjdvnkjdnfvksndvndsjkvnnesergeeriegbijreijbijbjLorem nkjdvnkjdnfvksndvndsjkvnnesergeeriegbijreijbijbjLorem nkjdvnkjdnfvksndvndsjkvnnesergeeriegbijreijbijbjLorem nkjdvnkjdnfvksndvndsjkvnnesergeeriegbijreijbijbjLorem nkjdvnkjdnfvksndvndsjkvnnesergeeriegbijreijbijbjLorem nkjdvnkjdnfvksndvndsjkvnnesergeeriegbijreijbijbjLorem nkjdvnkjdnfvksndvndsjkvnnesergeeriegbijreijbijbjLorem nkjdvnkjdnfvksndvndsjkvnnesergeeriegbijreijbijbjLorem nkjdvnkjdnfvksndvndsjkvnnesergeeriegbijreijbijbjLorem nkjdvnkjdnfvksndvndsjkvnnesergeeriegbijreijbijbjLorem nkjdvnkjdnfvksndvndsjkvnnesergeeriegbijreijbijbjLorem nkjdvnkjdnfvksndvndsjkvnnesergeeriegbijreijbijbjLorem nkjdvnkjdnfvksndvndsjkvnnesergeeriegbijreijbijbjLorem nkjdvnkjdnfvksndvndsjkvnnesergeeriegbijreijbijbjLorem nkjdvnkjdnfvksndvndsjkvnnesergeeriegbijreijbijbjLorem nkjdvnkjdnfvksndvndsjkvnnesergeeriegbijreijbijbjLorem nkjdvnkjdnfvksndvndsjkvnnesergeeriegbijreijbijbjLorem nkjdvnkjdnfvksndvndsjkvnnesergeeriegbijreijbijbj
+                                </p>
+                            </div>
+                        </div>
+                        <div style={{backgroundColor: "#1f1f1f", width: "auto", height: "1px", margin: "0 20px"}}></div>
+                    </UserComment>
+                </CommentBlock>
+            </div>
             <TranslateMvCard data={hardData} />
         </DashboardLayout>
     );

a3d44d45ed568c1c5373defd02094b38db613486
KhoaAnhNguyen04 <khoanguyenanh040104@gmail.com>
2025-01-07 09:36:31 +0700

    update search calling API (#40)
    Co-authored-by: Khoa <khoana@nexlesoft.com>

--- src/pages/SearchPage/SearchPage.js
+++ src/pages/SearchPage/SearchPage.js
@@ -1,12 +1,14 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import SearchResultCard from '~/components/SearchResultCard/SearchResultCard';
 import './SearchPage.css';
 import { SearchContext } from '~/context/SearchProvider';
 import narutoImg from 'src/assets/images/naruto.png';
 import onePieceImg from 'src/assets/images/one_piece.png';
-
+import axios from 'axios';
 
 export default function SearchPage() {
+    const apiUrl = process.env.REACT_APP_API_URL;
+
     const results = [
         { id: 1, img: narutoImg, title: 'Inception', category: 'Movie', rating: '4.8', year: 2010 },
         { id: 2, img: onePieceImg, title: 'Breaking Bad', category: 'TV Show', rating: '3.5', year: 2008 },
@@ -15,15 +17,30 @@ export default function SearchPage() {
         { id: 5, img: onePieceImg, title: 'Breaking Bad', category: 'TV Show', rating: '3.5', year: 2008 },
         { id: 6, img: narutoImg, title: 'The Godfather', category: 'Movie', rating: '4.2', year: 1972 },
     ];
-
     const { inputValue } = useContext(SearchContext);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`${apiUrl}/api/movies/search`, {
+                    params: {
+                        title: 'a',
+                    },
+                });
+                console.log(response.data);
+            } catch (err) {
+            } finally {
+            }
+        };
+        fetchData();
+    }, []);
 
     const handleAddToWishlist = ({ id }) => {
         alert('Added to Wishlist!');
     };
     const handleRemoveFromWishlist = ({ id }) => {
         alert('Removed from Wishlist!');
-    }
+    };
 
     return (
         <div className="search-result-page">
@@ -43,5 +60,5 @@ export default function SearchPage() {
                 ))}
             </div>
         </div>
-      );
+    );
 }
